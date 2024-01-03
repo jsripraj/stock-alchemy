b44240cabb05499cf89a898df389f5b519d03f77
joe <joesriprajittichai@gmail.com>
2024-01-03 14:50:11 -0500

    initial commit

---
+++ edgar.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Planet Fitness (PLNT) CIK: 1637207
+# curl -v -H "User-Agent: jsripraj@gmail.com" https://data.sec.gov/submissions/CIK0001637207.json
+# curl -v -H "User-Agent: jsripraj@gmail.com" https://data.sec.gov/api/xbrl/companyfacts/CIK0001637207.json
+curl -v -H "User-Agent: jsripraj@gmail.com" https://data.sec.gov/api/xbrl/companyfacts/CIK0001637207.json
