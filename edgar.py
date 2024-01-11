e19959146fb9cc1a74351f200ce1cba5af6dee78
joe <joesriprajittichai@gmail.com>
2024-01-10 19:24:28 -0800

    dump json output for jq parsing

--- edgar.py
+++ edgar.py
@@ -1,11 +1,18 @@
 import requests
 import pprint
+import json
 
+OUTPUT_FILE = "out.txt"
 url = 'https://data.sec.gov/api/xbrl/companyfacts/CIK0001637207.json'
 headers = {'user-agent': 'jsripraj@gmail.com'}
+
 r = requests.get(url, headers=headers)
 # print(f'status {r.status_code}')
 # print(r.text)
-with open('out.txt', 'w') as f:
-    f.write(pprint.pformat(r.json(), width=160))
+with open(OUTPUT_FILE, 'w') as f:
+    # f.write(pprint.pformat(r.json(), width=160))
+    # output = str(r.json())
+    # output.replace(r"'", r'"')
+    json_output = json.dumps(r.json())
+    f.write(json_output)
 # print(r.json())
