ef466177f9be74b505b9b48c7d533eb0d6a94315
joe <joesriprajittichai@gmail.com>
2024-01-12 21:13:55 -0800

    create sheet in user's Google Drive

--- quickstart.py
+++ quickstart.py
@@ -6,9 +6,11 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import google.auth
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+# SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
 # SAMPLE_SPREADSHEET_ID = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
@@ -42,28 +44,52 @@ def main():
 
   try:
     service = build("sheets", "v4", credentials=creds)
+    create("test-report", service)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-        .execute()
-    )
-    values = result.get("values", [])
+  #   # Call the Sheets API
+  #   sheet = service.spreadsheets()
+  #   result = (
+  #       sheet.values()
+  #       .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+  #       .execute()
+  #   )
+  #   values = result.get("values", [])
 
-    if not values:
-      print("No data found.")
-      return
+  #   if not values:
+  #     print("No data found.")
+  #     return
 
-    # print("Name, Major:")
-    print("Got data from sample sheet:")
-    for row in values:
-      # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[1]}, {row[2]}")
+  #   # print("Name, Major:")
+  #   print("Got data from sample sheet:")
+  #   for row in values:
+  #     # Print columns A and E, which correspond to indices 0 and 4.
+  #     print(f"{row[0]}, {row[1]}, {row[2]}")
   except HttpError as err:
     print(err)
 
+
+def create(title, service):
+  """
+  Creates the Sheet the user has access to.
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  # creds, _ = google.auth.default()
+  # pylint: disable=maybe-no-member
+  try:
+    # service = build("sheets", "v4", credentials=creds)
+    spreadsheet = {"properties": {"title": title}}
+    spreadsheet = (
+        service.spreadsheets()
+        .create(body=spreadsheet, fields="spreadsheetId")
+        .execute()
+    )
+    print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+    return spreadsheet.get("spreadsheetId")
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    return error
 
 if __name__ == "__main__":
   main()
