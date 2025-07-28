f7d5bba093afa52d16666a9d919a087f13b5f192
jsripraj <jsripraj@gmail.com>
2025-07-27 17:21:32 -0700

    createFIdToData now adds a Q4 item, in addition to Year and the three Quarter items

--- concepts.py
+++ concepts.py
@@ -1,17 +1,26 @@
 from enum import Enum, auto
 
-class PeriodType(Enum):
-    Quarter = auto()
+class Duration(Enum):
+    OneQuarter = auto()
+    TwoQuarters = auto()
+    ThreeQuarters = auto()
     Year = auto()
     Other = auto()
 
 class Concepts(Enum):
     Revenue = auto()
     NetIncome = auto()
+    CashFlowFromOperatingActivities = auto()
+    CashFlowFromInvestingActivities = auto()
+    CashFlowFromFinancingActivities = auto()
 
 aliasToConcept = {
     'RevenueFromContractWithCustomerExcludingAssessedTax': Concepts.Revenue,
     'Revenues': Concepts.Revenue,
     'SalesRevenueNet': Concepts.Revenue,
     'NetIncomeLoss' : Concepts.NetIncome,
+    'NetCashProvidedByUsedInOperatingActivities' : Concepts.CashFlowFromOperatingActivities,
+    'NetCashProvidedByUsedInInvestingActivities' : Concepts.CashFlowFromInvestingActivities,
+    'NetCashProvidedByUsedInFinancingActivities' : Concepts.CashFlowFromFinancingActivities,
+
 }
--- update_financials.py
+++ update_financials.py
@@ -5,7 +5,7 @@ import json
 from pprint import pp
 import mysql.connector
 from collections import defaultdict
-from datetime import datetime
+import datetime
 import config
 import concepts
 from enum import Enum
@@ -20,53 +20,89 @@ class FiscalValue:
         return f"FiscalValue('{self.concept}', '{self.alias}', '{self.value}')"
 
 class FiscalFinancial:
-    def __init__(self, cik: str, fId: str, periodType: Enum, end: str, accn: str, form: str, fy: str , fp: str):
+    def __init__(self, cik: str, fId: str, duration: Enum, start: str, end: str, accn: str, form: str, fy: str , fp: str):
         self.cik: str = cik
         self.fId: str = fId # fiscal ID
-        self.periodType: Enum = periodType
+        self.duration: Enum = duration
+        self.start: str = start
         self.end: str = end
         self.accn: str = accn # Accession number
         self.form: str = form
         self.fy: str = fy
         self.fp: str = fp
-        self.values: dict[str, list[FiscalValue]] = defaultdict(list)
+        self.values: dict[str, list[FiscalValue]] = defaultdict(list) # concept to values
 
-def createFId(accn: str, end: str, periodType: Enum) -> str:
-    return "_".join([accn, end, periodType.name])
+def strToDate(dateStr: str) -> datetime:
+    return datetime.datetime.strptime(dateStr, "%Y-%m-%d")
+
+def dateToStr(date: datetime.datetime) -> str:
+    return date.strftime("%Y-%m-%d")
+
+def createFId(accn: str, end: str, duration: Enum) -> str:
+    '''
+    Returns a Fiscal ID: <accn>_<end date yyyy-mm-dd>_<duration (see concepts.py)>
+    '''
+    return "_".join([accn, end, duration.name])
+
+def deconstructFId(fId: str) -> tuple[str, str, str]:
+    return fId.split('_')
 
 def createFIdToData(data: dict, cik: str) -> dict[str, dict] | None:
     '''
     Returns dict mapping ID to data dict. 
     '''
     if 'facts' not in data:
-        print(f'{fname}: facts NOT FOUND')
+        print(f'{cik}: facts NOT FOUND')
         return None
     if 'us-gaap' not in data['facts']:
-        print(f'{fname}: us-gaap NOT FOUND')
+        print(f'{cik}: us-gaap NOT FOUND')
         return None
-    if 'Assets' not in data['facts']['us-gaap']:
-        print(f'{fname}: Assets not found')
+    # if 'Assets' not in data['facts']['us-gaap']:
+    #     print(f'{cik}: Assets not found')
+    #     return None
+    if 'NetIncomeLoss' not in data['facts']['us-gaap']:
+        print(f'{cik}: NetIncomeLoss not found')
         return None
 
     accnToEntry = {}
-    for entry in data['facts']['us-gaap']['Assets']['units']['USD']:
+    # for entry in data['facts']['us-gaap']['Assets']['units']['USD']:
+    for entry in data['facts']['us-gaap']['NetIncomeLoss']['units']['USD']:
         form = entry['form']
         if form == '10-K' or form == '10-Q':
             accn = entry['accn']
+            # Use the most recent entry for each accn
             if (accn not in accnToEntry) or (entry['end'] > accnToEntry[accn]['end']):
                 accnToEntry[accn] = entry
 
     fIdToFiscalFinancial = {}
+    ends = []
     for accn, entry in accnToEntry.items():
         start = entry['start'] if 'start' in entry else None
         end = entry['end']
         form = entry['form']
-        period = getPeriodType(start, end, form)
+        period = getDuration(start, end, form)
         accn = entry['accn']
         fId = createFId(accn, end, period)
         fiscalYear = entry['fy']
         fiscalPeriod = entry['fp']
-        fIdToFiscalFinancial[fId] = FiscalFinancial(cik, fId, period, end, accn, form, fiscalYear, fiscalPeriod)
+        fIdToFiscalFinancial[fId] = FiscalFinancial(cik, fId, period, start, end, accn, form, fiscalYear, fiscalPeriod)
+        ends.append(end)
+    
+    # Add the missing Q4s
+    ends.sort()
+    fIds = [fId for fId in fIdToFiscalFinancial.keys()]
+    for fId in fIds:
+        ff = fIdToFiscalFinancial[fId]
+        if ff.period == concepts.Duration.Year:
+            i = ends.index(ff.end)
+            if i == 0:
+                continue
+            priorQuarterEnd = strToDate(ends[i-1])
+            startDate = priorQuarterEnd + datetime.timedelta(days=1)
+            start = dateToStr(startDate)
+            duration = concepts.Duration.OneQuarter
+            quarterId = createFId(ff.accn, ff.end, duration)
+            fIdToFiscalFinancial[quarterId] = FiscalFinancial(cik, quarterId, duration, start, ff.end, ff.accn, None, None, None)
     return fIdToFiscalFinancial
 
 def getConcepts(data: dict, fIdToFiscalFinancial: dict) -> None:
@@ -79,34 +115,91 @@ def getConcepts(data: dict, fIdToFiscalFinancial: dict) -> None:
                 end = entry['end']
                 form = entry['form']
                 start = entry['start'] if 'start' in entry else None
-                period = getPeriodType(start, end, form)
+                period = getDuration(start, end, form)
                 entryId = createFId(accn, end, period)
                 if entryId in fIdToFiscalFinancial:
                     concept = concepts.aliasToConcept[alias].name
                     value = entry['val']
                     fIdToFiscalFinancial[entryId].values[concept].append(FiscalValue(concept, alias, value))
+    getMissingOneQuarterConcepts(fIdToFiscalFinancial)
+    handleConceptIssues(fIdToFiscalFinancial)
 
+def getMissingOneQuarterConcepts(fIdToFiscalFinancial: dict) -> None:
     for fId, ff in fIdToFiscalFinancial.items():
-        for concept in concepts.Concepts:
-            values = ff.values[concept.name]
-            if not values:
-                print(f'ID {fId}: No values found')
-            elif len(values) > 1:
-                print(f'ID {fId}: Too many values found: {values}')
-            else:
-                print(f'ID {fId}: One value found: {values[0]}')
+        if fId.endswith(concepts.Duration.OneQuarter.name):
+            # find the concepts that OneQuarter is missing, should be cash flow concepts
+            for concept in concepts.Concepts:
+                concept = concept.name
+                data = ff.values[concept]
+                if not data:
+                    elif ff.fp == 'Q2':
+                        # get id of twoquarter
+                        threeQuarterId = idSub + concepts.Duration.ThreeQuarters.name
+                        twoQuarterId = idSub + concepts.Duration.ThreeQuarters.name
+                        threeQuarterId = idSub + concepts.Duration.ThreeQuarters.name
 
-def getPeriodType(startDate: str | None, endDate: str, form: str) -> Enum:
+                    # try to calculate as TwoQuarter minus OneQuarter
+                    # get id of twoquarter w same end date
+                    # get onequarter end date
+                    # get id of onequarter
+
+def getOneQuarterValue(ff: FiscalFinancial):
+    if ff.fp == 'Q3':
+        # try to calculate as ThreeQuarter minus TwoQuarter
+        # get id of threequarter w same end date
+        # get accn from fId
+        # get date from fId
+        accn, q3End, _ = deconstructFId(fId)
+        # use ThreeQuarter
+        q3Id = createFId(accn, q3End, concepts.Duration.ThreeQuarters)
+        # get twoquarter end date
+        q2End = strToDate(ff.start) - datetime.timedelta(days=1)
+        q2Id = createFId(accn, q2End, concepts.Duration.TwoQuarters)
+        q3Value = fIdToFiscalFinancial[q3Id].values[concept][0].value
+        q2Value = fIdToFiscalFinancial[q2Id].values[concept][0].value
+        data.append(q3Value - q2Value)
+
+
+def handleConceptIssues(fIdToFiscalFinancial: dict) -> None:
+    ciks = set()
+    for fId, ff in fIdToFiscalFinancial.items():
+        values = ff.values
+        for c in concepts.Concepts:
+            concept = c.name
+            data = values[concept]
+            if len(data) != 1:
+                print(f'CIK: {ff.cik}, FID: {fId}, Concept: {concept}, Msg: {len(data)} values found => {data}')
+                ciks.add(ff.cik)
+            # if len(data) == 1:
+            #     print(f'ID {fId}: One value found: {data}')
+    for cik in ciks:
+        fname = f'CIK{cik}.json'
+        extractZipFileToJson(fname)
+
+def getDuration(startDate: str | None, endDate: str, form: str) -> Enum:
     if not startDate:
-        return concepts.PeriodType.Quarter if form == "10-Q" else concepts.PeriodType.Year
-    start = datetime.strptime(startDate, "%Y-%m-%d")
-    end = datetime.strptime(endDate, "%Y-%m-%d")
-    days = (end - start).days
-    if 80 < days < 100:
-        return concepts.PeriodType.Quarter
-    if 330 < days < 380:
-        return concepts.PeriodType.Year
-    return concepts.PeriodType.Other
+        return concepts.Duration.OneQuarter if form == "10-Q" else concepts.Duration.Year
+    start = strToDate(startDate)
+    end = strToDate(endDate)
+    duration = (end - start).days
+    if 80 < duration < 100:
+        return concepts.Duration.OneQuarter
+    if 170 < duration < 190:
+        return concepts.Duration.TwoQuarters
+    if 260 < duration < 280:
+        return concepts.Duration.ThreeQuarters
+    if 330 < duration < 380:
+        return concepts.Duration.Year
+    return concepts.Duration.Other
+
+def extractZipFileToJson(filename: str):
+    # filename = "CIK0000320193.json" # Apple
+    with zipfile.ZipFile(config.ZIP_PATH, 'r') as zf:
+        with zf.open(filename) as inFile:
+            content = inFile.read()
+            data = json.loads(content.decode('utf-8'))
+            with open(os.path.join(config.DATA_DIR, filename), 'w') as outFile:
+                json.dump(data, outFile, indent=2)
 
 # Download companyfacts.zip
 # headers = {'User-Agent': config.EMAIL}
@@ -118,36 +211,32 @@ def getPeriodType(startDate: str | None, endDate: str, form: str) -> Enum:
 #             if chunk:
 #                 f.write(chunk)
 
-cnx = mysql.connector.connect(host=config.MYSQL_HOST, database=config.MYSQL_DATABASE, user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"))
-cursor = cnx.cursor()
-query = ("SELECT CIK FROM companies")
-cursor.execute(query)
-cursor.fetchall()
-# for cik in cursor:
-#     cik = cik[0]
-for cik in ['0000320193', '0002011641']: # Apple, Ferguson Enterprises
-    fname = 'CIK' + cik + '.json'
-    with zipfile.ZipFile(config.ZIP_PATH, 'r') as z:
-        with open("test.txt", 'w') as write_file:
-            try:
-                with z.open(fname) as f:
-                    content = f.read()
-                    data = json.loads(content.decode('utf-8'))
-                    fIdToData = createFIdToData(data, cik)
-                    if fIdToData:
-                        print(f'\nCIK: {cik}')
-                        getConcepts(data, fIdToData)
-            except KeyError as e:
-                print(f'KeyError: {e}')
+def run():
+    cnx = mysql.connector.connect(host=config.MYSQL_HOST, database=config.MYSQL_DATABASE, user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"))
+    cursor = cnx.cursor()
+    query = ("SELECT CIK FROM companies;")
+    cursor.execute(query)
+    # cursor.fetchall()
+    # for cik in ['0000320193', '0002011641']: # Apple, Ferguson Enterprises
+    for cik in cursor:
+        cik = cik[0]
+        print(f'\nCIK: {cik}')
+        fname = 'CIK' + cik + '.json'
+        with zipfile.ZipFile(config.ZIP_PATH, 'r') as z:
+            with open("test.txt", 'w') as write_file:
+                try:
+                    with z.open(fname) as f:
+                        content = f.read()
+                        data = json.loads(content.decode('utf-8'))
+                        fIdToData = createFIdToData(data, cik)
+                        if fIdToData:
+                            getConcepts(data, fIdToData)
+                except KeyError as e:
+                    print(f'KeyError: {e}')
 
-cnx.commit()
-cursor.close()
-cnx.close()
+    cnx.commit()
+    cursor.close()
+    cnx.close()
 
-# fname = "CIK0000320193.json" # Apple
-# with zipfile.ZipFile(config.ZIP_PATH, 'r') as z:
-#     with z.open(fname) as f:
-#         content = f.read()
-#         data = json.loads(content.decode('utf-8'))
-#         with open("apple.json", 'w') as write_file:
-#             json.dump(data, write_file, indent=2)
+if __name__ == "__main__":
+    run()
