e028c48a187264884cf3681e7b58c24914e74766
jsripraj <jsripraj@gmail.com>
2025-09-04 09:49:33 -0700

    added round brackets to ops

--- next-app/app/components/FormulaBuilder.tsx
+++ next-app/app/components/FormulaBuilder.tsx
@@ -27,7 +27,7 @@ export default function FormulaBuilder({
             {n}
           </button>
         ))}
-        {["+", "-", "*", "/", "<", ">"].map((op) => (
+        {["+", "-", "*", "/", "(", ")", "<", ">"].map((op) => (
           <button
             key={op}
             className="px-3 py-1 bg-blue-200 rounded"
