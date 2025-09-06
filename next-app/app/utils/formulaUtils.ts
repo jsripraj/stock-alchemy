eabc6393c2b827d419add52cec6b877244c17083
jsripraj <jsripraj@gmail.com>
2025-09-05 18:02:44 -0700

    created getSqlQuery

--- next-app/app/utils/formulaUtils.ts
+++ next-app/app/utils/formulaUtils.ts
@@ -1,32 +1,122 @@
 export function formatConcept(words: string[]): string {
-    return `[${words.join(" ")}]`;
+  return `[${words.join(" ")}]`;
 }
 
-export function extractTokens(formula: string): Set<string> {
-    const regex = /\[[^\]]+\]/g;
-    return new Set(formula.match(regex));
+function extractTokens(formula: string): Set<string> {
+  const regex = /\[[^\]]+\]/g;
+  return new Set(formula.match(regex));
 }
 
-export function getSqlName(token: string): string {
-    const bracketsRegex = /[\[\]]/g;
-    const words = token.replaceAll(bracketsRegex, "").split(" ");
-    const year = words[0];
-    const concept= words.slice(1).join("");
-    return `${concept}${year}`;
+function capitalizeFirstLetter(word: string): string {
+  if (!word) return word;
+  const chars = word.split("");
+  chars[0] = chars[0].toUpperCase();
+  return chars.join("");
 }
 
-export function getSqlSelectTerm(token: string, mostRecentYear: string): string {
-    if (token === "[Market Cap]") {
-        return `(companies.close * ${getSqlSelectTerm("[2024 Shares Outstanding]", mostRecentYear)})`;
-    }
-    return `${getSqlName(token)}.value`
+function extractConceptName(token: string): string {
+  const bracketsRegex = /[\[\]]/g;
+  const words = token.replaceAll(bracketsRegex, "").split(" ");
+  return words
+    .slice(1)
+    .map((word) => capitalizeFirstLetter(word))
+    .join("");
 }
 
-export function getSqlJoinStatement(token: string, mostRecentYear: string): string {
-    if (token === "[MarketCap]") {
-        return getSqlJoinStatement("[2024 Shares Outstanding]", mostRecentYear);
-    }
-    return `
-        join financ
-    `
+function extractYear(token: string): string {
+  const bracketsRegex = /[\[\]]/g;
+  const words = token.replaceAll(bracketsRegex, "").split(" ");
+  return words[0];
+}
+
+function getSqlName(token: string): string {
+  if (token === "[Market Cap]") {
+    return "MarketCap";
+  }
+  const year = extractYear(token);
+  const concept = extractConceptName(token);
+  return `${concept}${year}`;
+}
+
+function getSqlSelectTerm(
+  token: string,
+  mostRecentYear: string
+): string {
+  if (token === "[Market Cap]") {
+    return `(companies.close * ${getSqlSelectTerm(
+      "[2024 Shares Outstanding]",
+      mostRecentYear
+    )})`;
+  }
+  return `${getSqlName(token)}.value`;
+}
+
+function getSqlJoinStatement(
+  token: string,
+  mostRecentYear: string
+): string {
+  if (token === "[Market Cap]") {
+    return getSqlJoinStatement(`[${mostRecentYear} Shares Outstanding]`, mostRecentYear);
+  }
+  const concept = getSqlName(token);
+  return `join financials ${concept} on companies.cik = ${concept}.cik
+            and ${concept}.concept = '${extractConceptName(token)}'
+            and ${concept}.year = ${extractYear(token)}
+            and ${concept}.period = 'Q4'
+            and (${concept}.duration = 'Year' or ${concept}.duration is null)`;
+}
+
+function getSqlSelectExpression(
+  formula: string,
+  tokens: Set<string>,
+  mostRecentYear: string
+) {
+  tokens.forEach((token) => {
+    formula = formula.replaceAll(
+      token,
+      getSqlSelectTerm(token, mostRecentYear)
+    );
+  });
+  return formula;
+}
+
+export function getSqlQuery(formula: string, mostRecentYear: string) {
+  const tokens = extractTokens(formula);
+
+  const regex = /(.+)(<|>)(.+)/;
+  const match = formula.match(regex);
+
+  if (!match) return null;
+
+  const [, leftFormula, compOperator, rightFormula] = match.map((s) =>
+    s.trim()
+  );
+  const leftSelect = getSqlSelectExpression(
+    leftFormula,
+    tokens,
+    mostRecentYear
+  );
+  const rightSelect = getSqlSelectExpression(
+    rightFormula,
+    tokens,
+    mostRecentYear
+  );
+  const joinStatements = [...tokens].map((token) =>
+    getSqlJoinStatement(token, mostRecentYear)
+  );
+
+  return `
+    with results as (
+        select
+            companies.ticker, 
+            companies.company,
+            ${leftSelect} as left,
+            ${rightSelect} as right
+        from companies
+        ${joinStatements.join("\n")}
+    )  
+    select *
+    from results
+    where left ${compOperator} right;
+  `;
 }
--- next-app/app/utils/postgresUtils.ts
+++ next-app/app/utils/postgresUtils.ts
@@ -1,19 +1,16 @@
 "use server";
 
 import postgres from "postgres";
-import { extractTokens, getSqlSelectTerm} from "@/app/utils/formulaUtils";
+import { getSqlQuery } from "@/app/utils/formulaUtils";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchResults(formula: string, dates: string[]) {
-  const tokens = extractTokens(formula);
+  const query = getSqlQuery(formula, dates[0]);
+  console.log(query);
 
-  const test = getSqlSelectTerm("[2024 Net Income]", dates[0]);
-  console.log(test);
 
   const data = await sql`
         select * from companies;
     `;
-  // console.log("hello");
-  //   console.log(data);
 }
