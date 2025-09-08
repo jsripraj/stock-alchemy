784a3114eaeae4a90fed486882dc1027905c528a
jsripraj <jsripraj@gmail.com>
2025-09-08 16:58:05 -0700

    separated Find Stocks button component

---
+++ next-app/app/components/FindStocksButton.tsx
@@ -0,0 +1,21 @@
+import { storeFormula } from "@/app/utils/postgresUtils";
+import { useRouter } from "next/navigation";
+
+export default function FindStocksButton({ formula }: { formula: string }) {
+  const router = useRouter();
+
+  return (
+    <div className="flex-1">
+      <button
+        className="px-3 py-1 bg-green-200 rounded"
+        onClick={async () => {
+          // TODO: check formula
+          const [{ id }] = await storeFormula(formula);
+          router.push(`/results?id=${id}`);
+        }}
+      >
+        Find Stocks
+      </button>
+    </div>
+  );
+}
