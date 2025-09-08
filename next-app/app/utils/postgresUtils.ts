2a9ef32d5880d7691698d40d37fafd02f9c19e4b
jsripraj <jsripraj@gmail.com>
2025-09-08 10:16:03 -0700

    changed fetchResults param to mostRecentYear

--- next-app/app/utils/postgresUtils.ts
+++ next-app/app/utils/postgresUtils.ts
@@ -5,8 +5,8 @@ import { getSqlQuery } from "@/app/utils/formulaUtils";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-export async function fetchResults(formula: string, dates: string[]) {
-  const query = getSqlQuery(formula, dates[0]);
+export async function fetchResults(formula: string, mostRecentYear: string) {
+  const query = getSqlQuery(formula, mostRecentYear);
 
   if (query) {
     const data = await sql.unsafe(query);
