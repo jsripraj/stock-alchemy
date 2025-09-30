f0dd42787a3c2080a7b91fdfe5918bf7c6dbe426
jsripraj <jsripraj@gmail.com>
2025-09-30 16:14:53 -0700

    moved logo and centering container div to default layout

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import { Sansation } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const asimovian = localFont({
+  src: "./fonts/Asimovian-Regular.ttf",
+});
 
 const sansation = Sansation({
   weight: "300",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "StockAlchemy",
@@ -19,10 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${sansation.className} antialiased p-0 m-0`}
-      >
-        {children}
+      <body className={`${sansation.className} antialiased p-0 m-0`}>
+        <div className="w-screen h-screen flex flex-col items-center overflow-hidden">
+          <div className="flex my-6 items-center">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={50}
+              height={50}
+              className="object-contain"
+            />
+            <h1 className={`${asimovian.className} text-4xl text-lime-500`}>
+              StockAlchemy
+            </h1>
+          </div>
+          {children}
+        </div>
       </body>
     </html>
   );
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -5,12 +5,6 @@ import Spreadsheet from "@/app/components/Spreadsheet";
 import FormulaBuilder from "@/app/components/FormulaBuilder";
 import FindStocksButton from "@/app/components/FindStocksButton";
 import { getMostRecentYear } from "@/app/utils/formulaUtils";
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const asimovian = localFont({
-  src: "./fonts/Asimovian-Regular.ttf",
-});
 
 const lastYear = getMostRecentYear();
 const dates = [...Array(10)].map((_, i) => (lastYear - i).toString());
@@ -57,44 +51,30 @@ export default function Home() {
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center overflow-hidden">
-      <div className="flex my-6 items-center">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={50}
-          height={50}
-          className="object-contain"
-        />
-        <h1 className={`${asimovian.className} text-4xl text-lime-500`}>
-          StockAlchemy
-        </h1>
-      </div>
-      <div className="flex-1 w-8/10 flex flex-col items-center overflow-hidden">
-        <Spreadsheet
-          dates={dates}
-          concepts={concepts}
-          cursorPosRef={cursorPosRef}
-          insertIntoFormula={insertIntoFormula}
-        />
-        <FormulaBuilder
-          formula={formula}
-          insertIntoFormula={insertIntoFormula}
-          setFormula={setFormula}
-          cursorPosRef={cursorPosRef}
-          dates={dates}
-          concepts={concepts}
-          errorMessage={errorMessage}
-          isMessageVisable={isMessageVisable}
-        />
-        <FindStocksButton
-          formula={formula}
-          dates={dates}
-          concepts={concepts}
-          setErrorMessage={setErrorMessage}
-          startMessageTimer={startMessageTimer}
-        />
-      </div>
+    <div className="flex-1 w-8/10 flex flex-col items-center overflow-hidden">
+      <Spreadsheet
+        dates={dates}
+        concepts={concepts}
+        cursorPosRef={cursorPosRef}
+        insertIntoFormula={insertIntoFormula}
+      />
+      <FormulaBuilder
+        formula={formula}
+        insertIntoFormula={insertIntoFormula}
+        setFormula={setFormula}
+        cursorPosRef={cursorPosRef}
+        dates={dates}
+        concepts={concepts}
+        errorMessage={errorMessage}
+        isMessageVisable={isMessageVisable}
+      />
+      <FindStocksButton
+        formula={formula}
+        dates={dates}
+        concepts={concepts}
+        setErrorMessage={setErrorMessage}
+        startMessageTimer={startMessageTimer}
+      />
     </div>
   );
 }
