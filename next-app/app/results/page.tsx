15bfccfc0e992e9c08f4cefaa35729b4ad9944e2
jsripraj <jsripraj@gmail.com>
2025-09-30 21:14:18 -0700

    added empty div above results for formula

--- next-app/app/results/page.tsx
+++ next-app/app/results/page.tsx
@@ -9,6 +9,7 @@ export default async function ResultsPage({ searchParams }: ResultsPageProps) {
   const sp = await searchParams;
   const formulaId = sp.id;
   const noResults = <p>No Results</p>;
+
   if (!formulaId) {
     return noResults;
   }
@@ -24,35 +25,46 @@ export default async function ResultsPage({ searchParams }: ResultsPageProps) {
   const headers = ["Ticker", "Company", "Left Side", "Right Side"];
 
   return (
-    <table className="border-separate border-spacing-0">
-      <thead>
-        <tr>
-          {headers.map((h) => (
-            <th
-              key={h}
-              scope="col"
-              className={`border border-[#a0a0a0] px-[10px] py-2 sticky top-0 left-0 bg-white z-20`}
-            >
-              {h}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {results.map((res) => (
-          <tr key={res.ticker}>
-            {Object.entries(res).map(([key, value]) => (
-              <td
-                key={key}
-                scope="row"
-                className={`border border-[#a0a0a0] px-[10px] py-2 text-lime-500`}
+    <div>
+      <div
+        className="w-full flex-1 mb-2 p-2 
+          border border-lime-500 focus:border-2 focus:border-lime-400 rounded-xs outline-none 
+          overflow-y-auto 
+          text-lime-50 text-lg font-mono 
+          scrollbar scrollbar-thumb-stone-600 scrollbar-track-lime-500
+        "
+      >
+      </div>
+      <table className="border-separate border-spacing-0">
+        <thead>
+          <tr>
+            {headers.map((h) => (
+              <th
+                key={h}
+                scope="col"
+                className={`border border-[#a0a0a0] px-[10px] py-2 sticky top-0 left-0 bg-white z-20`}
               >
-                {value}
-              </td>
+                {h}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {results.map((res) => (
+            <tr key={res.ticker}>
+              {Object.entries(res).map(([key, value]) => (
+                <td
+                  key={key}
+                  scope="row"
+                  className={`border border-[#a0a0a0] px-[10px] py-2 text-lime-500`}
+                >
+                  {value}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
