6ae69da18f155dc0c45a81828231aa6820de5fca
Joe S <jsripraj@gmail.com>
2026-01-15 11:22:01 -0800

    feat: change font to remove font override warning

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Sansation } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/layout/Header";
 
-const sansation = Sansation({
-  weight: "300",
+const geist = Geist({
   subsets: ["latin"],
+  weight: "200"
 });
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${sansation.className} antialiased p-0 m-0`}>
+      <body className={`${geist.className} antialiased p-0 m-0`}>
         <div className="w-screen h-screen flex flex-col items-center overflow-hidden">
           <Header />
           {children}
